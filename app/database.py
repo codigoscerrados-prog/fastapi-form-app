d58cf4b185f0fee3bbb3324c1a528f8d137dc756
John Flores <johnflores@email.com>
2025-08-11 21:37:05 -0500

    Subir proyecto completo

--- app/database.py
+++ app/database.py
@@ -4,9 +4,25 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
+# Cargar variables de entorno
 load_dotenv()
 
+# Leer la URL de conexión
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Validar que no esté vacía
+if not DATABASE_URL:
+    raise ValueError(
+        "❌ No se encontró la variable DATABASE_URL. "
+        "Crea un archivo .env en la raíz del proyecto con:\n"
+        "DATABASE_URL=postgresql://jfm:jHSiUmhq6FPsSgoZ9fkNCMYpuo5kBgzo@dpg-d29nkbk9c44c73estt3g-a/dbformularioexample_7vlb"
+    )
+
+# Crear motor
 engine = create_engine(DATABASE_URL)
+
+# Sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base para modelos
 Base = declarative_base()

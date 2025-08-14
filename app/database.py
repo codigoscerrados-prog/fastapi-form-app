dec61bed598717332a863c5f74ac3ae164e36d08
John Flores <johnflores@email.com>
2025-08-13 21:00:57 -0500

    Subir proyecto completo

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("postgresql://jfm:jHSiUmhq6FPsSgoZ9fkNCMYpuo5kBgzo@dpg-d29nkbk9c44c73estt3g-a.oregon-postgres.render.com/dbformularioexample_7vlb")  # Ejemplo: "postgresql://usuario:clave@localhost:5432/tu_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
